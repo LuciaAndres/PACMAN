051e756c9026bbbb6d44e8ac5c081f66c1b20524
UnstableLuca <lucas.joglar@gmail.com>
2024-12-23 13:10:19 +0100

    primer test

--- pacman.js
+++ pacman.js
@@ -1,1 +1,16 @@
+class Mundo
+{
+    constructor(){
+    this.canvas = document.getElementById("juego");
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx.drawImage("mapa.png",0,0);
+    }
+}
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const image = document.getElementById("source");
+
+image.addEventListener("load", (e) => {
+  ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+});
