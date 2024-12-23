7b31970a4eda7a6abcf9ef0076c897b076ec08d5
UnstableLuca <lucas.joglar@gmail.com>
2024-12-23 13:19:58 +0100

    Test 1

--- pacman.js
+++ pacman.js
@@ -7,8 +7,8 @@ class Mundo
     }
 }
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("juego");
+const ctx = this.canvas.getContext("2d");
 const image = document.getElementById("source");
 
 image.addEventListener("load", (e) => {
